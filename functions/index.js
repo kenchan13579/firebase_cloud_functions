0a478a08a7f66ed82ac1b29e491c66aaf359aabb
Leung Chan <kenchan13579@hotmail.com>
2018-06-03 09:29:28 -0700

    genreate family code for invitation

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,7 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const randomatic = require('randomatic');
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +9,21 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+
+// TODO: Remove expired codes, duplicate codes
+exports.generateFamilyCode = functions.https.onRequest((req, res) => {
+    const {familyId} = req.query;
+    const code = randomatic('0', 6);
+    const d = new Date();
+    d.setDate(d.getDate() + 1);
+    if (familyId === undefined) {
+        throw new functions.https.HttpsError('invalid-argument', 'familyId is required')
+    }
+    return admin.firestore().collection('invite_codes').add({
+        family_id: familyId,
+        code,
+        expired_date: d
+    }).then(() => res.end('done'))
+    .catch(err => res.status(500).end(err))
+})
