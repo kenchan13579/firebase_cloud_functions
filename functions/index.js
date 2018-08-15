3330ab3b2242bebb7c58762e85e36ca48fd11009
Leung Chan <kenchan13579@hotmail.com>
2018-08-14 21:06:59 -0700

    push notification on new comments

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,19 @@ const admin = require("firebase-admin");
 const randomatic = require("randomatic");
 
 admin.initializeApp();
+const COLLECTION_NAMES = {
+  CHATS: "chats",
+
+  FAMILIES: "families",
+
+  POST_COMMENT: "post-comment",
+
+  POST_IDS: "posts",
+
+  POSTS_DATA: "posts-data",
+
+  USERS: "users"
+};
 
 exports.generateFamilyCode = functions.https.onCall((data, context) => {
   const { familyId } = data;
@@ -58,6 +71,147 @@ exports.generateFamilyCode = functions.https.onCall((data, context) => {
       throw new functions.https.HttpsError(err.code, err.message, err.details);
     });
 });
+exports.pushOnNewEmotion = functions.firestore
+  .document(`/${COLLECTION_NAMES.POSTS_DATA}/{postId}`)
+  .onUpdate((change, context) => {
+    const newValue = change.after.data();
+    const previousValue = change.before.data();
+    console.log(newValue, previousValue);
+    if (!newValue.comments && !previousValue.comments) {
+      return Promise.resolve();
+    }
+    if (
+      newValue.comments &&
+      previousValue.comments &&
+      Object.keys(newValue.comments).length <=
+        Object.keys(previousValue.comments).length
+    ) {
+      return Promise.resolve();
+    }
+    let newCommentId = Object.keys(newValue.comments).filter(
+      id => !previousValue.comments || !previousValue.comments[id]
+    )[0];
+    console.log(newCommentId, "newcomid");
+    let getPostAuthor = admin
+      .firestore()
+      .doc(`/${COLLECTION_NAMES.USERS}/${newValue.author}`)
+      .get();
+    let getComment = admin
+      .firestore()
+      .doc(`/${COLLECTION_NAMES.POST_COMMENT}/${newCommentId}`)
+      .get();
+    return Promise.all([getPostAuthor, getComment])
+      .then(([postAuthor, commentData]) => {
+        if (!postAuthor || !postAuthor.exists) {
+          return Promise.reject(new Error("post author couldn't be fetched"));
+        }
+        if (!commentData || !commentData.exists) {
+          return Promise.reject(new Error("comment data not fetched"));
+        }
+        let commentAuthorId = commentData.get("authorID");
+        let getCommentAuthor = admin
+          .firestore()
+          .doc(`/${COLLECTION_NAMES.USERS}/${commentAuthorId}`)
+          .get();
+        return Promise.all([postAuthor, commentData, getCommentAuthor]);
+      })
+      .then(([postAuthor, commentData, commentAuthorData]) => {
+        if (!commentAuthorData || !commentAuthorData.exists) {
+          return Promise.reject(
+            new Error("comment author couldn't be fetched")
+          );
+        }
+        const commentAuthorName = getFirstName(commentAuthorData.get("name"));
+        const commentText = commentData.get("text");
+        const payload = {
+          notification: {
+            title: `${commentAuthorName} posted a comment on your feed`,
+            body: `"${commentText}"`
+          }
+        };
+        console.log("author", postAuthor.get("fcmToken"));
+        console.log(payload);
+
+        return admin
+          .messaging()
+          .sendToDevice([postAuthor.get("fcmToken")], payload);
+      });
+  });
+exports.pushOnNewComment = functions.firestore
+  .document(`/${COLLECTION_NAMES.POSTS_DATA}/{postId}`)
+  .onUpdate((change, context) => {
+    const newValue = change.after.data();
+    const previousValue = change.before.data();
+    console.log(newValue, previousValue);
+    if (!newValue.comments && !previousValue.comments) {
+      return Promise.resolve();
+    }
+    if (
+      newValue.comments &&
+      previousValue.comments &&
+      Object.keys(newValue.comments).length <=
+        Object.keys(previousValue.comments).length
+    ) {
+      return Promise.resolve();
+    }
+    let newCommentId = Object.keys(newValue.comments).filter(
+      id => !previousValue.comments || !previousValue.comments[id]
+    )[0];
+    console.log(newCommentId, "newcomid");
+    let getPostAuthor = admin
+      .firestore()
+      .doc(`/${COLLECTION_NAMES.USERS}/${newValue.author}`)
+      .get();
+    let getComment = admin
+      .firestore()
+      .doc(`/${COLLECTION_NAMES.POST_COMMENT}/${newCommentId}`)
+      .get();
+    return Promise.all([getPostAuthor, getComment])
+      .then(([postAuthor, commentData]) => {
+        if (!postAuthor || !postAuthor.exists) {
+          return Promise.reject(new Error("post author couldn't be fetched"));
+        }
+        if (!commentData || !commentData.exists) {
+          return Promise.reject(new Error("comment data not fetched"));
+        }
+        let commentAuthorId = commentData.get("authorID");
+        let getCommentAuthor = admin
+          .firestore()
+          .doc(`/${COLLECTION_NAMES.USERS}/${commentAuthorId}`)
+          .get();
+        return Promise.all([postAuthor, commentData, getCommentAuthor]);
+      })
+      .then(([postAuthor, commentData, commentAuthorData]) => {
+        if (!commentAuthorData || !commentAuthorData.exists) {
+          return Promise.reject(
+            new Error("comment author couldn't be fetched")
+          );
+        }
+        const commentAuthorName = getFirstName(commentAuthorData.get("name"));
+        const commentText = commentData.get("text");
+        const payload = {
+          notification: {
+            title: `${commentAuthorName} posted a comment on your feed`,
+            body: `"${commentText}"`
+          }
+        };
+        console.log("author", postAuthor.get("fcmToken"));
+        console.log(payload);
+
+        return admin
+          .messaging()
+          .sendToDevice([postAuthor.get("fcmToken")], payload);
+      });
+  });
+
+/**
+ *
+ * @param {string} name
+ * @return {string}
+ */
+function getFirstName(name = "") {
+  return name.split(" ")[0] || "User";
+}
 
 exports.pushNewFeedsToFamilyMembers = functions.firestore
   .document("/posts/{familyId}")
@@ -77,11 +231,11 @@ exports.pushNewFeedsToFamilyMembers = functions.firestore
 
     const getPostData = admin
       .firestore()
-      .doc(`/posts-data/${newPostIds[0]}`)
+      .doc(`/${COLLECTION_NAMES.POSTS_DATA}/${newPostIds[0]}`)
       .get();
     const getFamilyMembers = admin
       .firestore()
-      .doc(`/families/${familyId}`)
+      .doc(`/${COLLECTION_NAMES.FAMILIES}/${familyId}`)
       .get();
 
     return Promise.all([getPostData, getFamilyMembers])
@@ -101,7 +255,7 @@ exports.pushNewFeedsToFamilyMembers = functions.firestore
         const userRequests = members.map(id =>
           admin
             .firestore()
-            .doc(`/users/${id}`)
+            .doc(`/${COLLECTION_NAMES.USERS}/${id}`)
             .get()
         );
         return Promise.all([authorId, ...userRequests]);
